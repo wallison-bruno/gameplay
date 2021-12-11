f8d25cfa9decc2ddf4068025145b4f3a3d8fe8dd
Wallison Bruno Moura Cruz <63119956+wallison-bruno@users.noreply.github.com>
2021-12-11 10:49:40 -0300

    first commit

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { style } from "./styles";
+import { View, Text } from "react-native";
+export function Signin() {
+    return (
+        <View style={style.container}>
+            <Text>
+                Wallison Bruno!!!
+            </Text>
+        </View>
+    );
+}
